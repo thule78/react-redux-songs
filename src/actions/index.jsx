456f9a59d45f67b4a362e9406b34dbd298a90b60
thule <lunthu@gmail.com>
2019-06-06 12:28:17 +0700

    add redux

---
+++ src/actions/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h2>actions/index</h2>
+    </div>
+
+    )
+}
+
+export default Index
---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Index from '../actions/index';
+
+const App = () =>{
+  return (
+    <div className="container">
+
+      <h1>Hello Songs</h1>
+    </div>
+    )
+}
+
+export default App
